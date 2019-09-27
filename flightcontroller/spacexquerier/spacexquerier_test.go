8b41047fe33b9ba83607ce5fbdf701d5747bae0c
Houssem El Fekih <hassoum92@hotmail.com>
2019-09-27 17:19:29 +0100

    end of day commit, starting to prepare tests for spaceX queries

--- flightcontroller/spacexquerier/spacexquerier_test.go
+++ flightcontroller/spacexquerier/spacexquerier_test.go
@@ -1,8 +1,25 @@
 package spacexquerier_test
 
-import "testing"
+import (
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
 
 func TestBooks(t *testing.T) {
 	RegisterFailHandler(Fail)
 	RunSpecs(t, "Scheduler suite")
 }
+
+var _ = Describe("SpaceX api integration", func() {
+	BeforeEach(func() {
+		newSpaceXServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			if r.RequestURI == "/getall" {
+				bytebuff, _ := ioutil.ReadFile("fixtures/getall.json")
+				w.Header().Set("Content-Type", "text/json")
+				w.Write(bytebuff)
+			}
+		}))
+	})
+})
