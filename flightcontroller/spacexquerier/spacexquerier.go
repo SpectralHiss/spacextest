c35c01808dc3e360f888513b1f0a77cc289da646
Houssem El Fekih <hassoum92@hotmail.com>
2019-09-28 15:26:05 +0100

    gone launchid impl doing launchpossible

--- flightcontroller/spacexquerier/spacexquerier.go
+++ flightcontroller/spacexquerier/spacexquerier.go
@@ -1,7 +1,15 @@
 package spacexquerier
 
 import (
+	"encoding/json"
+
+	"log"
+	"net/http"
+	"net/url"
 	"time"
+
+	//"fmt"
+
 
 	"github.com/SpectralHiss/spacextest/flightcontroller/querytypes"
 	. "github.com/SpectralHiss/spacextest/flightcontroller/querytypes"
@@ -11,4 +19,47 @@ import (
 type SpaceXQuerier interface {
 	LaunchPossible(launchID LaunchPadID, date time.Time) bool
 	GetLaunchIds() []querytypes.LaunchPadID
+}
+
+type SpaceXQueryImpl struct {
+	ApiUrl string
+}
+
+func (sq*SpaceXQueryImpl) LaunchPossible(launchID LaunchPadID, date time.Time) bool {
+	return true
+}
+
+func (sq *SpaceXQueryImpl) GetLaunchIds() []LaunchPadID {
+	u, err := url.Parse(sq.ApiUrl)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	u.Path += "/v3/landpads"
+	Response, err := http.Get(u.String())
+	if err != nil || Response.StatusCode != 200 {
+		return []LaunchPadID{}
+	}
+
+	dec := json.NewDecoder(Response.Body)
+
+	type LaunchDetails struct {
+		SiteID LaunchPadID `json:"site_id"`
+	}
+
+	var lids []LaunchDetails
+	for dec.More() {
+		err = dec.Decode(&lids)
+
+		if err != nil {
+			return []LaunchPadID{}
+		}
+	}
+	
+	collectLaunchIDs := []LaunchPadID{}
+	for _, elem := range lids {
+		collectLaunchIDs = append(collectLaunchIDs, elem.SiteID)
+	}
+
+	return collectLaunchIDs
 }
--- flightcontroller/spacexquerier/spacexquerier_test.go
+++ flightcontroller/spacexquerier/spacexquerier_test.go
@@ -5,21 +5,87 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"testing"
+	"time"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	"github.com/SpectralHiss/spacextest/flightcontroller/spacexquerier"
+
+	. "github.com/SpectralHiss/spacextest/flightcontroller/querytypes"
 )
 
-func TestBooks(t *testing.T) {
+func TestSpcxApi(t *testing.T) {
 	RegisterFailHandler(Fail)
 	RunSpecs(t, "Scheduler suite")
 }
 
 var _ = Describe("SpaceX api integration", func() {
-	BeforeEach(func() {
-		newSpaceXServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			if r.RequestURI == "/getall" {
-				bytebuff, _ := ioutil.ReadFile("fixtures/getall.json")
+
+	var tServer *httptest.Server
+	var spaceXQ spacexquerier.SpaceXQueryImpl
+
+	BeforeSuite(func() {
+
+		tServer = httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			if r.RequestURI == "/v3/landpads" {
+				bytebuff, _ := ioutil.ReadFile("./json_responses/getall.json")
+				w.Header().Set("Content-Type", "text/json")
+				w.Write(bytebuff)
+			}
+			if r.RequestURI == "/v3/launches/upcoming" {
+				bytebuff, _ := ioutil.ReadFile("./json_responses/upcoming.json")
 				w.Header().Set("Content-Type", "text/json")
 				w.Write(bytebuff)
 			}
 		}))
+		spaceXQ.ApiUrl = tServer.URL
 	})
-})
+
+	AfterSuite(func() {
+		tServer.Close()
+	})
+	Describe("GetLaunchIds",func(){
+		Context("When GetLaunchIds is called", func() {
+
+			It("Should return the right launchIds", func() {
+				
+				expectedLaunchIds := []LaunchPadID{
+					"vafb_slc_4e",
+					"kwajalein_atoll",
+					"ccafs_slc_40",
+					"stls",
+					"ksc_lc_39a",
+					"vafb_slc_3w",
+				}
+	
+				Expect(spaceXQ.GetLaunchIds()).To(Equal(expectedLaunchIds))
+			})
+		})
+	
+		//TODO make error ret not doen for interest of time
+		Context("If spaceX unavailable, it returns an empty list", func() {
+		})
+	
+	})
+
+
+	Describe("Check if conflicts with spacexlaunch",func(){
+		
+		Context("when called with a conflicting date for a launchpadID",func(){
+			It("Should return false",func(){
+				aDateOfLaunch , err := time.Parse(time.RFC3339, "2019-02-22T01:45:00.000Z")
+				Expect(err).To(BeNil())
+				ret := spaceXQ.LaunchPossible(LaunchPadID("ccafs_slc_40"),aDateOfLaunch)
+				Expect(ret).To(BeFalse())
+			})
+
+		})
+	})
+
+
+
+
+		  
+	})
+
